6d5481aaf052fb4c96e4b8db37ae375291518478
Rick Taylor <rick@tropicalstormsoftware.com>
2024-05-09 10:06:02 +0100

    Use tracing not simple logging

--- bpa/src/logger.rs
+++ bpa/src/logger.rs
@@ -4,15 +4,10 @@ pub fn init(config: &config::Config) {
     let log_level: String = settings::get_with_default(config, "log_level", "info")
         .expect("Failed to find 'log_level' in config");
 
-    let log_level = log_level
-        .parse::<simplelog::LevelFilter>()
-        .expect("Invalid log level");
+    let log_level = log_level.parse::<tracing_subscriber::filter::LevelFilter>().expect("Invalid log level");
 
-    simplelog::TermLogger::init(
-        log_level,
-        simplelog::Config::default(),
-        simplelog::TerminalMode::Mixed,
-        simplelog::ColorChoice::Auto,
-    )
-    .expect("Failed to configure logging")
+    tracing_subscriber::fmt()
+        .with_max_level(log_level)
+        .with_target(false)
+        .init();
 }
