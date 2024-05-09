18717576940b50766b0053a7a24e1aa071b4c131
Rick Taylor <rick@tropicalstormsoftware.com>
2024-05-09 14:34:23 +0100

    cargo fmt

--- bpa/src/logger.rs
+++ bpa/src/logger.rs
@@ -4,7 +4,9 @@ pub fn init(config: &config::Config) {
     let log_level: String = settings::get_with_default(config, "log_level", "info")
         .expect("Failed to find 'log_level' in config");
 
-    let log_level = log_level.parse::<tracing_subscriber::filter::LevelFilter>().expect("Invalid log level");
+    let log_level = log_level
+        .parse::<tracing_subscriber::filter::LevelFilter>()
+        .expect("Invalid log level");
 
     tracing_subscriber::fmt()
         .with_max_level(log_level)
