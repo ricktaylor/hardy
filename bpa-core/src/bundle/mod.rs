62b8c4038aa4890850d400c6fabbd9702ce09857
Rick Taylor <rick@tropicalstormsoftware.com>
2024-06-06 13:15:58 +0100

    Duplicate struct left over from old refactor

--- bpa-core/src/bundle/mod.rs
+++ bpa-core/src/bundle/mod.rs
@@ -21,34 +21,10 @@ pub use block_type::BlockType;
 pub use bundle_flags::BundleFlags;
 pub use bundle_id::{BundleId, FragmentInfo};
 pub use bundle_status::BundleStatus;
+pub use bundle_type::{Bundle, HopInfo};
 pub use crc::CrcType;
 pub use creation_timestamp::CreationTimestamp;
 pub use eid::{Eid, EidError};
 pub use eid_pattern::{EidPattern, EidPatternError};
 pub use eid_pattern_map::EidPatternMap;
 pub use metadata::Metadata;
-
-#[derive(Default, Debug)]
-pub struct Bundle {
-    // From Primary Block
-    pub id: BundleId,
-    pub flags: BundleFlags,
-    pub crc_type: CrcType,
-    pub destination: Eid,
-    pub report_to: Eid,
-    pub lifetime: u64,
-
-    // Unpacked from extension blocks
-    pub previous_node: Option<Eid>,
-    pub age: Option<u64>,
-    pub hop_count: Option<HopInfo>,
-
-    // The extension blocks
-    pub blocks: std::collections::HashMap<u64, Block>,
-}
-
-#[derive(Debug, Copy, Clone)]
-pub struct HopInfo {
-    pub count: u64,
-    pub limit: u64,
-}
