fb29175a49a79e43b1f071fb3c80fc76c276dc07
Rick Taylor <rick@tropicalstormsoftware.com>
2024-06-06 12:45:48 +0100

    Clippy

--- bpa-core/fuzz/fuzz_targets/eid_cbor.rs
+++ bpa-core/fuzz/fuzz_targets/eid_cbor.rs
@@ -1,9 +1,8 @@
 #![no_main]
 
-use libfuzzer_sys::fuzz_target;
 use hardy_cbor::decode::FromCbor;
+use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
-    let _ =
-        hardy_bpa_core::bundle::Eid::from_cbor(data);
+    let _ = hardy_bpa_core::bundle::Eid::from_cbor(data);
 });
