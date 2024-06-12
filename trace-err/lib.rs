ed84e0783b08b99125f52ddb989c88b24ab3ddeb
Rick Taylor <rick@tropicalstormsoftware.com>
2024-06-12 09:54:36 +0100

    Only need core::fmt::Debug, and use core not std

--- trace-err/lib.rs
+++ trace-err/lib.rs
@@ -1,17 +1,17 @@
 // Shamelessly derived from the log_err crate
 
-pub trait TraceErrResult<T, E: std::fmt::Debug + std::fmt::Display> {
+pub trait TraceErrResult<T, E: core::fmt::Debug> {
     #[track_caller]
     fn trace_expect(self, msg: &str) -> T;
 }
 
-impl<T, E: std::fmt::Debug + std::fmt::Display> TraceErrResult<T, E> for std::result::Result<T, E> {
+impl<T, E: core::fmt::Debug> TraceErrResult<T, E> for core::result::Result<T, E> {
     /// `expect`s the `Result`, and outputs error message (in exact same style as `expect`) through `error!` as well.
     fn trace_expect(self, msg: &str) -> T {
         match self {
             Ok(v) => v,
             Err(ref e) => {
-                tracing::error!(target: "expect","{}: {msg}: {e}",std::panic::Location::caller());
+                tracing::error!(target: "expect","{}: {msg}: {e:?}",core::panic::Location::caller());
                 self.expect(msg)
             }
         }
@@ -23,13 +23,13 @@ pub trait TraceErrOption<T> {
     fn trace_expect(self, msg: &str) -> T;
 }
 
-impl<T> TraceErrOption<T> for std::option::Option<T> {
+impl<T> TraceErrOption<T> for core::option::Option<T> {
     /// `expect`s the `Option`, and outputs error message (in exact same style as `expect`) through `error!` as well.
     fn trace_expect(self, msg: &str) -> T {
         match self {
             Some(n) => n,
             None => {
-                tracing::error!(target:"expect","{}: {msg}",std::panic::Location::caller());
+                tracing::error!(target:"expect","{}: {msg}",core::panic::Location::caller());
                 self.expect(msg)
             }
         }
