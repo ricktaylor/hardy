63ebfeb8cfc18fb5a829b12d0861e4b09d92da34
Rick Taylor <rick@tropicalstormsoftware.com>
2024-07-21 16:19:10 +0100

    Work out how to produce coverage!

--- bpv7/fuzz/fuzz_targets/bundle.rs
+++ bpv7/fuzz/fuzz_targets/bundle.rs
@@ -6,3 +6,5 @@ use libfuzzer_sys::fuzz_target;
 fuzz_target!(|data: &[u8]| {
     let _ = hardy_bpv7::prelude::ValidBundle::from_cbor(data);
 });
+
+// llvm-cov show --format=html  -instr-profile ./fuzz/coverage/bundle/coverage.profdata ./target/x86_64-unknown-linux-gnu/coverage/x86_64-unknown-linux-gnu/release/bundle -o ./fuzz/coverage/bundle/ -ignore-filename-regex='/.cargo/|rustc/'
