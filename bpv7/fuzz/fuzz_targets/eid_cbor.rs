3df40f30971d2453993958bedee4e50d7e295416
Rick Taylor <rtaylor@aalyria.com>
2024-08-22 12:35:51 +0000

    Silence clippy

--- bpv7/fuzz/fuzz_targets/eid_cbor.rs
+++ bpv7/fuzz/fuzz_targets/eid_cbor.rs
@@ -4,6 +4,6 @@ use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
     if let Ok(eid) = hardy_cbor::decode::parse::<hardy_bpv7::prelude::Eid>(data) {
-        format!("{eid:?}");
+        _ = format!("{eid:?}");
     }
 });
--- cbor/fuzz/fuzz_targets/decode.rs
+++ cbor/fuzz/fuzz_targets/decode.rs
@@ -4,7 +4,7 @@ use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
     _ = hardy_cbor::decode::try_parse_value(data, |value, _| {
-        format!("{value:?}");
+        _ = format!("{value:?}");
         Ok::<_, hardy_cbor::decode::Error>(())
     });
 });
