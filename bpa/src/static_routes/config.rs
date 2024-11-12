b0e621d7b556eeeb963bbcbce906db4ee9fc2eb9
Rick Taylor <rtaylor@aalyria.com>
2024-11-12 18:31:46 +0000

    Cosmetics

--- bpa/src/static_routes/config.rs
+++ bpa/src/static_routes/config.rs
@@ -19,26 +19,22 @@ pub struct Config {
 
 impl Config {
     pub fn new(config: &::config::Config) -> Option<Self> {
-        if let Some(mut config) =
+        let mut config =
             settings::get_with_default::<Option<config::Config>, _>(config, "static_routes", None)
-                .trace_expect("Invalid 'static_routes' section in configuration")
-        {
-            // Try to create canonical file path
-            if let Ok(r) = config.routes_file.canonicalize() {
-                config.routes_file = r;
-            }
+                .trace_expect("Invalid 'static_routes' section in configuration")?;
 
-            // Ensure it's absolute
-            if config.routes_file.is_relative() {
-                let mut path =
-                    std::env::current_dir().trace_expect("Failed to get current directory");
-                path.push(&config.routes_file);
-                config.routes_file = path;
-            }
-            Some(config)
-        } else {
-            None
+        // Try to create canonical file path
+        if let Ok(r) = config.routes_file.canonicalize() {
+            config.routes_file = r;
         }
+
+        // Ensure it's absolute
+        if config.routes_file.is_relative() {
+            let mut path = std::env::current_dir().trace_expect("Failed to get current directory");
+            path.push(&config.routes_file);
+            config.routes_file = path;
+        }
+        Some(config)
     }
 
     fn default_path() -> PathBuf {
