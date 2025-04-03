b628b402dbca82d52d521b5d0495be66be48787c
Rick Taylor <rtaylor@aalyria.com>
2025-04-03 11:31:23 +0000

    Big rework of static_routes parser

--- bpa-server/src/static_routes/parse.rs
+++ bpa-server/src/static_routes/parse.rs
@@ -1,63 +1,58 @@
 use super::*;
 use winnow::{
     ModalResult, Parser,
-    ascii::{Caseless, dec_uint, line_ending, space0, space1},
-    combinator::{alt, eof, opt, repeat_till, separated_pair},
+    ascii::{Caseless, dec_uint, line_ending, space0, space1, till_line_ending},
+    combinator::{alt, opt, separated, separated_pair},
     stream::AsChar,
-    token::take_till,
+    token::{rest, take_till},
 };
 
 fn parse_priority(input: &mut &[u8]) -> ModalResult<u32> {
-    (space1, Caseless("priority"), space1, dec_uint)
-        .map(|(_, _, _, v)| v)
+    (Caseless("priority"), space1, dec_uint)
+        .map(|(_, _, v)| v)
         .parse_next(input)
 }
 
-fn parse_drop(input: &mut &[u8]) -> ModalResult<(Action, Option<u32>)> {
-    (
-        (
-            Caseless("drop"),
-            opt((space1, dec_uint.try_map(|v: u64| v.try_into()))),
-        )
-            .map(|v| Action::Drop(v.1.map(|v| v.1))),
-        opt(parse_priority),
+fn parse_drop(input: &mut &[u8]) -> ModalResult<Action> {
+    ((
+        Caseless("drop"),
+        opt((space1, dec_uint.try_map(|v: u64| v.try_into()))),
     )
-        .parse_next(input)
+        .map(|(_, v)| Action::Drop(v.map(|v| v.1))))
+    .parse_next(input)
 }
 
-fn parse_via(input: &mut &[u8]) -> ModalResult<(Action, Option<u32>)> {
-    (
-        (separated_pair(
-            Caseless("via"),
-            space1,
-            take_till(1.., |c: u8| c.is_space() || c.is_newline()).parse_to(),
-        )
-        .map(|v| Action::Via(v.1))),
-        opt(parse_priority),
+fn parse_via(input: &mut &[u8]) -> ModalResult<Action> {
+    (separated_pair(
+        Caseless("via"),
+        space1,
+        take_till(1.., AsChar::is_space).parse_to(),
     )
-        .parse_next(input)
+    .map(|(_, v)| Action::Via(v)))
+    .parse_next(input)
 }
 
-fn parse_store(input: &mut &[u8]) -> ModalResult<(Action, Option<u32>)> {
-    (
-        (separated_pair(
-            (Caseless("store"), opt((space1, Caseless("until")))),
-            space1,
-            take_till(1.., |c: u8| c.is_space() || c.is_newline()).try_map(|s| {
-                time::OffsetDateTime::parse(
-                    &String::from_utf8_lossy(s),
-                    &time::format_description::well_known::Rfc3339,
-                )
-            }),
-        )
-        .map(|v| Action::Store(v.1))),
-        opt(parse_priority),
+fn parse_store(input: &mut &[u8]) -> ModalResult<Action> {
+    (separated_pair(
+        (Caseless("store"), opt((space1, Caseless("until")))),
+        space1,
+        take_till(1.., AsChar::is_space).try_map(|s| {
+            time::OffsetDateTime::parse(
+                &String::from_utf8_lossy(s),
+                &time::format_description::well_known::Rfc3339,
+            )
+        }),
     )
-        .parse_next(input)
+    .map(|(_, v)| Action::Store(v)))
+    .parse_next(input)
 }
 
 fn parse_action(input: &mut &[u8]) -> ModalResult<(Action, Option<u32>)> {
-    alt((parse_drop, parse_via, parse_store)).parse_next(input)
+    (
+        alt((parse_drop, parse_via, parse_store)),
+        opt((space1, parse_priority).map(|(_, v)| v)),
+    )
+        .parse_next(input)
 }
 
 fn parse_pattern(input: &mut &[u8]) -> ModalResult<bpv7::EidPattern> {
@@ -67,33 +62,24 @@ fn parse_pattern(input: &mut &[u8]) -> ModalResult<bpv7::EidPattern> {
 }
 
 fn parse_route(input: &mut &[u8]) -> ModalResult<(bpv7::EidPattern, StaticRoute)> {
-    (
-        space0,
-        separated_pair(parse_pattern, space1, parse_action),
-        space0,
-        line_ending,
-    )
+    separated_pair(parse_pattern, space1, parse_action)
+        .map(|(pattern, (action, priority))| (pattern, StaticRoute { priority, action }))
         .parse_next(input)
-        .map(|(_, (pattern, (action, priority)), _, _)| (pattern, StaticRoute { priority, action }))
+}
+
+fn parse_line(input: &mut &[u8]) -> ModalResult<Option<(bpv7::EidPattern, StaticRoute)>> {
+    alt((
+        (space0, opt(parse_route), space0).map(|(_, v, _)| v),
+        ('#', rest).map(|_| None),
+    ))
+    .parse_next(input)
 }
 
 #[allow(clippy::type_complexity)]
 fn parse_routes(input: &mut &[u8]) -> ModalResult<Vec<(bpv7::EidPattern, StaticRoute)>> {
-    repeat_till(
-        0..,
-        alt((
-            (parse_route, line_ending).map(|v| Some(v.0)),
-            ('#', take_till(0.., AsChar::is_newline)).map(|_| None),
-            (space0, line_ending).map(|_| None),
-        )),
-        eof,
-    )
-    .map(
-        |(v, _): (Vec<Option<(bpv7::EidPattern, StaticRoute)>>, &[u8])| {
-            v.into_iter().flatten().collect()
-        },
-    )
-    .parse_next(input)
+    separated(0.., till_line_ending.and_then(parse_line), line_ending)
+        .map(|v: Vec<Option<(bpv7::EidPattern, StaticRoute)>>| v.into_iter().flatten().collect())
+        .parse_next(input)
 }
 
 pub async fn load_routes(
@@ -127,5 +113,37 @@ pub async fn load_routes(
             Err(e) => Err(anyhow::format_err!("{e}").into()),
             Ok(v) => Ok(v),
         },
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test() {
+        parse_routes
+            .parse(b"ipn:*.*.* via ipn:0.1.0")
+            .expect("Failed");
+
+        parse_routes
+            .parse(b"dtn://**/** store until 2025-01-02T11:12:13Z priority 1200")
+            .expect("Failed");
+
+        parse_routes.parse(b"#").expect("Failed");
+        parse_routes.parse(b"#\n").expect("Failed");
+        parse_routes.parse(b"#      ").expect("Failed");
+        parse_routes.parse(b"#      \n").expect("Failed");
+
+        parse_routes.parse(b"").expect("Failed");
+        parse_routes.parse(b"\n").expect("Failed");
+        parse_routes.parse(b"      ").expect("Failed");
+        parse_routes.parse(b"      \n").expect("Failed");
+
+        parse_routes.parse(b"   \n   \n   ").expect("Failed");
+
+        parse_routes
+            .parse(b"ipn:*.*.* via ipn:0.1.0\ndtn://**/** store 2025-01-02T11:12:13Z priority 1200")
+            .expect("Failed");
     }
 }
