2fa38ea0c8dd3b713bc6a739e8c51a58fe7f9c04
Rick Taylor <rtaylor@aalyria.com>
2025-05-08 13:05:52 +0000

    Remove Display impl

--- bpv7/src/error.rs
+++ bpv7/src/error.rs
@@ -24,10 +24,10 @@ pub enum Error {
     #[error("Bundle has more than one block with block number {0}")]
     DuplicateBlockNumber(u64),
 
-    #[error("{1} block cannot be block number {0}")]
+    #[error("{1:?} block cannot be block number {0}")]
     InvalidBlockNumber(u64, BlockType),
 
-    #[error("Bundle has multiple {0} blocks")]
+    #[error("Bundle has multiple {0:?} blocks")]
     DuplicateBlocks(BlockType),
 
     #[error("Bundle source has no clock, and there is no Bundle Age extension block")]
