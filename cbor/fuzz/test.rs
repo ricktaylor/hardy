0e8f87cd93a089b3ceacb1f4dafdb785bd8ea202
Rick Taylor <rtaylor@aalyria.com>
2025-05-30 14:21:58 +0000

    Add fuzzing tests to test suite

---
+++ cbor/fuzz/test.rs
@@ -0,0 +1,33 @@
+#![cfg(test)]
+
+use std::io::Read;
+
+#[test]
+fn test_all() {
+    match std::fs::read_dir("./corpus/decode") {
+        Err(e) => {
+            eprintln!(
+                "Failed to open dir: {e}, curr dir: {}",
+                std::env::current_dir().unwrap().to_string_lossy()
+            );
+        }
+        Ok(dir) => {
+            for entry in dir {
+                if let Ok(entry) = entry {
+                    let path = entry.path();
+                    if path.is_file() {
+                        if let Ok(mut file) = std::fs::File::open(&path) {
+                            let mut buffer = Vec::new();
+                            if file.read_to_end(&mut buffer).is_ok() {
+                                _ = hardy_cbor::decode::try_parse_value(&buffer, |value, _, _| {
+                                    _ = format!("{value:?}");
+                                    Ok::<_, hardy_cbor::decode::Error>(())
+                                });
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
