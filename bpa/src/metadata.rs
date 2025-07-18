35dbcf7094c915722a0251610d1941385c6cab93
Rick Taylor <rtaylor@aalyria.com>
2025-07-18 12:06:06 +0000

    Serialize Metadata

--- bpa/src/metadata.rs
+++ bpa/src/metadata.rs
@@ -1,14 +1,24 @@
 use super::*;
+use serde::{Deserialize, Serialize};
+use serde_with::{
+    base64::{Base64, UrlSafe},
+    formats::Unpadded,
+    serde_as,
+};
 
-#[derive(Debug, Default, Clone)]
+#[serde_as]
+#[derive(Debug, Default, Clone, Serialize, Deserialize)]
 pub struct BundleMetadata {
     pub status: BundleStatus,
     pub storage_name: Option<Arc<str>>,
+
+    #[serde_as(as = "Option<Base64<UrlSafe, Unpadded>>")]
     pub hash: Option<Arc<[u8]>>,
+
     pub received_at: Option<time::OffsetDateTime>,
 }
 
-#[derive(Debug, Default, Clone, Eq, PartialEq)]
+#[derive(Debug, Default, Clone, Eq, PartialEq, Serialize, Deserialize)]
 pub enum BundleStatus {
     #[default]
     DispatchPending,
