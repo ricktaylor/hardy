be7ba1e3771b82fe8721de43898f18d115a70abe
Rick Taylor <rtaylor@aalyria.com>
2025-08-01 22:39:07 +0000

    Fix fuzz comment

--- bpa/fuzz/fuzz_targets/service.rs
+++ bpa/fuzz/fuzz_targets/service.rs
@@ -4,5 +4,5 @@ use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| common::service::service_send(data));
 
-// cargo cov -- show --format=html  -instr-profile ./fuzz/coverage/storage/coverage.profdata ./target/x86_64-unknown-linux-gnu/coverage/x86_64-unknown-linux-gnu/release/cla -o ./fuzz/coverage/storage/ -ignore-filename-regex='/.cargo/|rustc/|/target/'
-// cargo cov -- export --format=lcov  -instr-profile ./fuzz/coverage/storage/coverage.profdata ./target/x86_64-unknown-linux-gnu/coverage/x86_64-unknown-linux-gnu/release/cla -ignore-filename-regex='/.cargo/|rustc/|/target/' > ./fuzz/coverage/storage/lcov.info
+// cargo cov -- show --format=html  -instr-profile ./fuzz/coverage/service/coverage.profdata ./target/x86_64-unknown-linux-gnu/coverage/x86_64-unknown-linux-gnu/release/service -o ./fuzz/coverage/service/ -ignore-filename-regex='/.cargo/|rustc/|/target/'
+// cargo cov -- export --format=lcov  -instr-profile ./fuzz/coverage/service/coverage.profdata ./target/x86_64-unknown-linux-gnu/coverage/x86_64-unknown-linux-gnu/release/service -ignore-filename-regex='/.cargo/|rustc/|/target/' > ./fuzz/coverage/service/lcov.info
