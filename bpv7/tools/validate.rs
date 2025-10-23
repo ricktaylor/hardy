60a401a0d313588c48d858f087a9de36e878bc0d
Rick Taylor <rtaylor@aalyria.com>
2025-10-23 09:33:10 +0000

    Return Err rather than ExitCode

--- bpv7/tools/validate.rs
+++ bpv7/tools/validate.rs
@@ -9,62 +9,66 @@ pub struct Command {
     files: Vec<String>,
 }
 
-fn parse<R: std::io::Read>(filename: Option<String>, mut input: std::io::BufReader<R>) -> bool {
+fn parse<R: std::io::Read>(
+    filename: Option<String>,
+    mut input: std::io::BufReader<R>,
+) -> anyhow::Result<()> {
     let mut bundle = Vec::new();
     input
         .read_to_end(&mut bundle)
-        .expect("Failed to read from input");
+        .map_err(|e| anyhow::anyhow!("Failed to read from input: {e}"))?;
 
-    match hardy_bpv7::bundle::ValidBundle::parse(&bundle, &hardy_bpv7::bpsec::key::EmptyStore) {
-        Ok(hardy_bpv7::bundle::ValidBundle::Valid(_, _)) => true,
-        Ok(hardy_bpv7::bundle::ValidBundle::Rewritten(_, _, _, non_canonical)) => {
-            if non_canonical {
-                println!(
-                    "{}Non-canonical, but semantically valid bundle",
-                    filename.map(|f| format!("{f}: ")).unwrap_or_default()
-                );
-                false
-            } else {
-                true
-            }
-        }
-        Ok(hardy_bpv7::bundle::ValidBundle::Invalid(_, _, error)) => {
-            eprintln!(
-                "{}Parser has had to guess at the content, but basically garbage: {error}",
-                filename.map(|f| format!("{f}: ")).unwrap_or_default()
-            );
-            false
-        }
-        Err(e) => {
-            eprintln!(
+    match hardy_bpv7::bundle::ValidBundle::parse(&bundle, &hardy_bpv7::bpsec::key::EmptyStore)
+        .map_err(|e| {
+            anyhow::anyhow!(
                 "{}Failed to parse bundle: {e}",
-                filename.map(|f| format!("{f}: ")).unwrap_or_default()
-            );
-            false
+                filename
+                    .as_ref()
+                    .map(|f| format!("{f}: "))
+                    .unwrap_or_default()
+            )
+        })? {
+        hardy_bpv7::bundle::ValidBundle::Valid(_, _) => Ok(()),
+        hardy_bpv7::bundle::ValidBundle::Rewritten(_, _, _, non_canonical) => {
+            (!non_canonical).then_some(()).ok_or(anyhow::anyhow!(
+                "{}Non-canonical, but semantically valid bundle",
+                filename
+                    .as_ref()
+                    .map(|f| format!("{f}: "))
+                    .unwrap_or_default()
+            ))
         }
+        hardy_bpv7::bundle::ValidBundle::Invalid(_, _, error) => Err(anyhow::anyhow!(
+            "{}Parser has had to guess at the content, but basically garbage: {error}",
+            filename
+                .as_ref()
+                .map(|f| format!("{f}: "))
+                .unwrap_or_default()
+        )),
     }
 }
 
-pub fn exec(args: Command) -> ExitCode {
+pub fn exec(args: Command) -> anyhow::Result<()> {
     let mut count_failed: usize = 0;
     if args.files.is_empty() {
-        if !parse(None, std::io::BufReader::new(std::io::stdin())) {
+        if let Err(e) = parse(None, std::io::BufReader::new(std::io::stdin())) {
+            eprintln!("{e}");
             count_failed = 1;
         }
     } else {
         for f in args.files {
             let input = std::io::BufReader::new(
-                std::fs::File::open(&f).expect("Failed to open input file"),
+                std::fs::File::open(&f)
+                    .map_err(|e| anyhow::anyhow!("Failed to open input file: {e}"))?,
             );
-            if !parse(Some(f), input) {
+            if let Err(e) = parse(Some(f), input) {
+                eprintln!("{e}");
                 count_failed = count_failed.saturating_add(1);
             }
         }
     }
 
-    if count_failed == 0 {
-        ExitCode::SUCCESS
-    } else {
-        ExitCode::FAILURE
-    }
+    (count_failed == 0)
+        .then_some(())
+        .ok_or(anyhow::anyhow!("No files to validate"))
 }
