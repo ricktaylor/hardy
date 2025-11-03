0fcab0dd01edb455d64810ad41e5c696782481f0
Rick Taylor <rtaylor@aalyria.com>
2025-11-03 22:50:43 +0000

    Add comments and a sample config

--- file-cla/src/lib.rs
+++ file-cla/src/lib.rs
@@ -13,11 +13,18 @@ use tracing::Instrument;
 mod cla;
 mod watcher;
 
+/// Configuration for the file-based Convergence Layer Adapter (CLA).
 #[derive(Clone, Debug, Default)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[cfg_attr(feature = "serde", serde(default))]
 pub struct Config {
+    /// The directory to watch for new files to be sent as bundles.
+    /// Each file in this directory is treated as a complete bundle and will be
+    /// dispatched to the BPA. After dispatch, the file is deleted.
     pub outbox: Option<PathBuf>,
+    /// A map of peer Endpoint IDs (EIDs) to their corresponding inbox directories.
+    /// When a bundle is to be forwarded to a peer, it will be written as a file
+    /// in the directory associated with that peer's EID.
     pub peers: HashMap<Eid, PathBuf>,
 }
 
@@ -26,6 +33,7 @@ struct ClaInner {
     inboxes: HashSet<String>,
 }
 
+/// The main struct for the file-based Convergence Layer Adapter (CLA).
 pub struct Cla {
     _name: String,
     config: Config,
@@ -35,6 +43,12 @@ pub struct Cla {
 }
 
 impl Cla {
+    /// Creates a new `Cla` instance.
+    ///
+    /// # Arguments
+    ///
+    /// * `name` - A name for this CLA instance.
+    /// * `config` - The configuration for this CLA.
     pub fn new(name: String, config: Config) -> Self {
         Self {
             config,
