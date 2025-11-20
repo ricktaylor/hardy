420cf05062211726275847be9c938abd59ccfafa
Théo Tchilinguirian <theo.tchlx@proton.me>
2025-11-20 22:09:00 +0100

    bpv7/tools: add process control flags argument to bundle create
    This commit allows multiple bundle processing control flags to be passed
    as CLI arguments separated by commas.
    The flags names is derived from the fields of the
    hardy_bpv7::bundle::Flags struct.
    
    Each ArgFlags enum value is translated into a u64, accumulated, and the
    accumulated value is converted into a Flags value using Flags' From<u64>
    implementation.

--- bpv7/tools/create.rs
+++ bpv7/tools/create.rs
@@ -1,6 +1,46 @@
 use super::*;
+use hardy_bpv7::bundle::Flags;
 use hardy_bpv7::crc::CrcType;
 use hardy_bpv7::eid::Eid;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, ValueEnum)]
+enum ArgFlags {
+    /// Specify bundle is a fragment
+    #[value(name = "isfrag")]
+    IsFragment = 1 << 0,
+
+    /// Specify ADU is an administrative record
+    #[value(name = "isadm")]
+    IsAdminRecord = 1 << 1,
+
+    /// Require bundle to not be fragmented
+    #[value(name = "nofrag")]
+    DoNotFragment = 1 << 2,
+
+    /// Request acknowledgement by application
+    #[value(name = "ack")]
+    AppAckRequested = 1 << 5,
+
+    /// Request status time in status reports
+    #[value(name = "time")]
+    ReportStatusTime = 1 << 6,
+
+    /// Request reception status reports
+    #[value(name = "rcv")]
+    ReceiptReportRequested = 1 << 14,
+
+    /// Request forwarding status reports
+    #[value(name = "fwd")]
+    ForwardReportRequested = 1 << 16,
+
+    /// Request delivery status reports
+    #[value(name = "dlv")]
+    DeliveryReportRequested = 1 << 17,
+
+    /// Request deletion status reports
+    #[value(name = "del")]
+    DeleteReportRequested = 1 << 18,
+}
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, ValueEnum)]
 pub enum ArgCrcType {
@@ -40,6 +80,10 @@ pub struct Command {
     #[arg(short, long, required = false, default_value = "")]
     output: io::Output,
 
+    /// One or more bundle processing control flags, seperated by ','
+    #[arg(short, long, value_delimiter = ',')]
+    flags: Vec<ArgFlags>,
+
     /// The CRC type of the bundle
     #[arg(short, long = "crc-type")]
     crc_type: Option<ArgCrcType>,
@@ -55,12 +99,22 @@ pub struct Command {
 
 impl Command {
     pub fn exec(self) -> anyhow::Result<()> {
+        // Accumulate bundle processing control flags
+        let flags: Option<Flags> = if self.flags.is_empty() {
+            None
+        } else {
+            Some(Flags::from(
+                self.flags.iter().map(|flag| *flag as u64).sum::<u64>(),
+            ))
+        };
+
         let crc_val = self.crc_type.map(|crc| crc as u64);
 
         let builder: hardy_bpv7::builder::Builder = hardy_bpv7::builder::BundleTemplate {
             source: self.source,
             destination: self.destination,
             report_to: self.report_to,
+            flags,
             crc_type: crc_val.map(CrcType::from),
             lifetime: {
                 if let Some(lifetime) = self.lifetime {
@@ -73,7 +127,6 @@ impl Command {
                 }
             },
             hop_limit: self.hop_limit,
-            ..Default::default()
         }
         .into();
 
