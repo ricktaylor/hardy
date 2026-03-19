9528dacbfcf76a1b99a38ad2a193923a981ec32d
Rick Taylor <rtaylor@aalyria.com>
2026-03-19 09:41:52 +0000

    Rebase changes
    Signed-off-by: Rick Taylor <rtaylor@aalyria.com>

--- bpa-server/src/main.rs
+++ bpa-server/src/main.rs
@@ -52,17 +52,16 @@ async fn init_storage(
         Some(cfg) => Some(match cfg {
             config::MetadataStorage::Memory(cfg) => hardy_bpa::storage::metadata_mem::new(cfg),
 
-                #[cfg(feature = "sqlite-storage")]
-                config::MetadataStorage::Sqlite(cfg) => hardy_sqlite_storage::new(cfg, upgrade_storage),
-    
+            #[cfg(feature = "sqlite-storage")]
+            config::MetadataStorage::Sqlite(cfg) => hardy_sqlite_storage::new(cfg, upgrade_storage),
+
             #[cfg(feature = "postgres-storage")]
-                config::MetadataStorage::Postgres(cfg) => {
+            config::MetadataStorage::Postgres(cfg) => {
                 hardy_postgres_storage::new(cfg, upgrade_storage)
                     .await
                     .trace_expect("Failed to connect to Postgres metadata store")
             }
         }),
-        },
     };
 
     let bundle_storage = match storage.bundle.as_ref() {
@@ -120,8 +119,7 @@ async fn main() -> anyhow::Result<()> {
 
 async fn inner_main(config: config::Config, cli: cli::Args) -> anyhow::Result<()> {
     let (metadata_storage, bundle_storage) =
-       
-        init_storage(&config.storage, cli.upgrade_storage).await?.await;
+        init_storage(&config.storage, cli.upgrade_storage).await?;
 
     let bpa_config = hardy_bpa::config::Config {
         lru_capacity: config.storage.lru_capacity,
