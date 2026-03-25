ec86fe9b8943286eccaf9c6389d831a923c218a6
Rick Taylor <rtaylor@aalyria.com>
2026-03-25 19:38:21 +0000

    Initial attempt - mostly working
    Signed-off-by: Rick Taylor <rtaylor@aalyria.com>

---
+++ tests/interop/NASA-cFS/docker/config/bpnode_mission_cfg.h
@@ -0,0 +1,25 @@
+/*
+ * Custom BPNode Mission Configuration for Hardy interop testing.
+ * Only change from default: PSP driver name → stcpsock_intf
+ * All EID values kept at defaults to pass BPLib validation.
+ */
+
+#ifndef BPNODE_MISSION_CFG_H
+#define BPNODE_MISSION_CFG_H
+
+#include "bpnode_interface_cfg.h"
+
+#define DEFAULT_UDP_CLA
+#define BPNODE_CLA_PSP_DRIVER_NAME "stcpsock_intf"
+
+/* All EID values match original defaults */
+#define BPNODE_EID_SERVICE_NUM_FOR_CHANNEL_0 42
+#define BPNODE_EID_SERVICE_NUM_FOR_CHANNEL_1 53
+#define BPNODE_EID_NODE_NUM_FOR_CONTACT_0    200
+#define BPNODE_EID_SERVICE_NUM_FOR_CONTACT_0 64
+#define BPNODE_EID_NODE_NUM_FOR_CONTACT_1    400
+#define BPNODE_EID_SERVICE_NUM_FOR_CONTACT_1 42
+#define BPNODE_EID_NODE_NUM_FOR_CONTACT_2    600
+#define BPNODE_EID_SERVICE_NUM_FOR_CONTACT_2 12
+
+#endif /* BPNODE_MISSION_CFG_H */
