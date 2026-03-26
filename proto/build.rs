daaae64de8fb4e8ad9f648cb90a11aa13e08ec22
Rick Taylor <rtaylor@aalyria.com>
2026-03-26 17:42:40 +0000

    Simplify proto build
    Signed-off-by: Rick Taylor <rtaylor@aalyria.com>

--- proto/build.rs
+++ proto/build.rs
@@ -1,25 +1,15 @@
-use std::path::Path;
-
-// Clone of tonic_build::compile_protos() to add --experimental_allow_proto3_optional
-fn compile_proto(proto: impl AsRef<Path>) -> std::io::Result<()> {
-    let proto_path: &Path = proto.as_ref();
-
-    // directory the main .proto file resides in
-    let proto_dir = proto_path
-        .parent()
-        .expect("proto file should reside in a directory");
-
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_prost_build::configure()
         .bytes(".")
-        .protoc_arg("--experimental_allow_proto3_optional") // for older systems
-        .compile_protos(&[proto_path], &[proto_dir])
-}
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    compile_proto("cla.proto")?;
-    compile_proto("service.proto")?;
-    compile_proto("routing.proto")?;
-    compile_proto("google/rpc/status.proto")?;
-
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .compile_protos(
+            &[
+                "cla.proto",
+                "service.proto",
+                "routing.proto",
+                "google/rpc/status.proto",
+            ],
+            &["."],
+        )?;
     Ok(())
 }
