ca76798557066b91f0d1cc183c44539e48346808
Sylvain Pierrot <spierrot@aalyria.com>
2026-04-07 12:37:03 +0000

    test: add tests for fragment assembly
    Signed-off-by: Sylvain Pierrot <spierrot@aalyria.com>

--- bpa/src/dispatcher/reassemble.rs
+++ bpa/src/dispatcher/reassemble.rs
@@ -1,11 +1,15 @@
-use super::*;
-use storage::adu_reassembly::ReassemblyResult;
+use hardy_bpv7::bundle::ParsedBundle;
+use tracing::debug;
+
+use super::Dispatcher;
+use crate::bundle::{Bundle, BundleMetadata, BundleStatus};
+use crate::storage::adu_reassembly::ReassemblyResult;
 
 impl Dispatcher {
-    pub async fn reassemble(&self, mut bundle: bundle::Bundle) {
+    pub async fn reassemble(&self, mut bundle: Bundle) {
         let (storage_name, data) = match self.store.adu_reassemble(&bundle).await {
             ReassemblyResult::NotReady => {
-                let status = bundle::BundleStatus::AduFragment {
+                let status = BundleStatus::AduFragment {
                     source: bundle.bundle.id.source.clone(),
                     timestamp: bundle.bundle.id.timestamp.clone(),
                 };
@@ -19,13 +23,13 @@ impl Dispatcher {
             ReassemblyResult::Done(storage_name, data) => (storage_name, data),
         };
 
-        let metadata = bundle::BundleMetadata {
+        let metadata = BundleMetadata {
             storage_name: Some(storage_name),
             ..Default::default()
         };
 
-        let parsed = hardy_bpv7::bundle::ParsedBundle::parse(&data, self.key_provider());
-        let Ok(hardy_bpv7::bundle::ParsedBundle { bundle, .. }) = parsed else {
+        let parsed = ParsedBundle::parse(&data, self.key_provider());
+        let Ok(ParsedBundle { bundle, .. }) = parsed else {
             debug!("Reassembled bundle is invalid: {}", parsed.unwrap_err());
 
             // TODO: Report this as a reception failure for all the fragments
@@ -37,7 +41,7 @@ impl Dispatcher {
             return;
         };
 
-        let bundle = bundle::Bundle { metadata, bundle };
+        let bundle = Bundle { metadata, bundle };
         self.store.insert_metadata(&bundle).await;
 
         // Box::pin breaks the type recursion; call inner directly (already in pool)
