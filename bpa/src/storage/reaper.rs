b791d2cfd89417fc12570cc2c928136705fe8378
Rick Taylor <rtaylor@aalyria.com>
2026-04-07 10:04:48 +0000

    Expired counter
    Signed-off-by: Rick Taylor <rtaylor@aalyria.com>

--- bpa/src/storage/reaper.rs
+++ bpa/src/storage/reaper.rs
@@ -150,6 +150,7 @@ impl Store {
                     .await
                     .inspect_err(|e| error!("Failed to get metadata from store: {e}"))
                 {
+                    metrics::counter!("bpa.bundle.expired").increment(1);
                     dispatcher
                         .drop_bundle(
                             bundle,
