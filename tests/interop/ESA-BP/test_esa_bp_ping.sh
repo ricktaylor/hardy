4da3a01b51fedf6c88ea7439bd7da81125936a05
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2026-07-31 16:26:48 +0200

    test(interop): bound bp ping startup and shutdown with an outer timeout
    Signed-off-by: hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>

--- tests/interop/ESA-BP/test_esa_bp_ping.sh
+++ tests/interop/ESA-BP/test_esa_bp_ping.sh
@@ -283,7 +283,7 @@ EOF
     log_step "Hardy pinging ESA-BP at ipn:$ESA_BP_NODE_NUM.7 via STCP..."
     echo ""
 
-    PING_OUTPUT=$("$BP_BIN" ping "ipn:$ESA_BP_NODE_NUM.7" \
+    PING_OUTPUT=$(timeout $((PING_COUNT * 2 + 40))s "$BP_BIN" ping "ipn:$ESA_BP_NODE_NUM.7" \
         --cla "$MTCP_BIN" \
         --cla-args "--config $TEST_DIR/cla_ping.toml" \
         --grpc-listen "[::1]:$HARDY_GRPC_PORT" \
